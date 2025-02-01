e71d51df83b52eaf216abcab18acb3b67a96f474
seif8911 <seif.islam.888@gmail.com>
2025-02-01 20:24:36 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,16 +8,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAYwExZcsbxK5-6tIuqybZCwA9k-o23aPs",
-  authDomain: "healthcare-9f289.firebaseapp.com",
-  projectId: "healthcare-9f289",
-  storageBucket: "healthcare-9f289.appspot.com",
-  messagingSenderId: "724732091273",
-  appId: "1:724732091273:web:e8668c43e37ca7c79a9f58",
-  measurementId: "G-YE8HQ5J9ZH"
+  apiKey: "AIzaSyDv_UFd-486l7V_er4QEAmq9itrf_q9V-k",
+  authDomain: "alert-vim-344418.firebaseapp.com",
+  projectId: "alert-vim-344418",
+  storageBucket: "alert-vim-344418.firebasestorage.app",
+  messagingSenderId: "138830808551",
+  appId: "1:138830808551:web:662adb6eb6f6ed48f3a942",
+  measurementId: "G-KGK71QFF7Z"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
 // const analytics = getAnalytics(app);
